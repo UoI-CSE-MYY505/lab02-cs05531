77725b7275d513f2610db8dd40740bf022f3fa14
cs05531 <cs05531@uoi.gr>
2024-10-21 19:49:00 +0300

    Completed lab02

--- lab02.s
+++ lab02.s
@@ -12,6 +12,28 @@ prog:
 #-----------------------------
 # Write your code here!
 # Do not remove the prog label or write code above it!
+
+    li s0, 0          
+
+    beq a1, zero, done  
+
+loop:
+    lw t0, 0(a0)      
+
+    beq t0, a2, update_s0  
+    
+    addi a0, a0, 4    
+    addi a1, a1, -1   
+    bnez a1, loop     
+
+    j done            
+
+update_s0:
+    add s0, zero, a0
+    addi a0, a0, 4    
+    addi a1, a1, -1   
+    bnez a1, loop 
+     
 #-----------------------------
 done:
     addi a7, zero, 10 
